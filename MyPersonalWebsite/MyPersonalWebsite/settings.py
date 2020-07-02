b4efce88badeaadb73d0c4c4c52ed10a9e50fca1
Fernando <flopezramirez@hotmail.com>
2020-07-01 23:50:09 -0500

    Set and divide project by apps

--- MyPersonalWebsite/MyPersonalWebsite/settings.py
+++ MyPersonalWebsite/MyPersonalWebsite/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'education',
+    'awards',
+    'blogs',
+    'core',
+    'experience',
+    'projects'
 ]
 
 MIDDLEWARE = [
